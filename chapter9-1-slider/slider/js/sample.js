7fb0f61679e5c5248131f60d4e66e182c73f12c9
鹿又勇太 <>
2021-12-23 18:40:19 +0900

    correction[css pointer,JS switching speed]

--- chapter9-1-slider/slider/js/sample.js
+++ chapter9-1-slider/slider/js/sample.js
@@ -21,25 +21,23 @@
   // 前へボタンをクリック時
   prev.addEventListener('click', function () {
     if (clickBtn === true) {
-      // clickBtnの真偽地がfalseの時だけクリックできない
+      // clickBtnの真偽値がfalseの時だけクリックできない
       clickBtn = false;
       // スライドショーのクラス（appear）を追加
       view.classList.add("anime");
       // current−１つ減;
       current--;
       // current0より小さくなったりしないようにif文で分岐
-      if (current < 0) {
-        // currentとsetImage.lengthの整合性を合わせるためにsetImage.lengthから-1
-        current = setImage.length - 1;
-      }
+      // currentとsetImage.lengthの整合性を合わせるためにsetImage.lengthから-1
+      if (current < 0) current = setImage.length - 1;
       // currentをインデックス番号として変数setImageの配列からcurrentのインデックス番号の画像を取り出す
       view.src = setImage[current];
       // スライドショーのクラス（anime）を外す
-      setTimeout('view.classList.remove("anime");', 3100);
+      setTimeout('view.classList.remove("anime");', 2100);
       setTimeout(function () {
-        // clickBtnの真偽地がtrueの時だけクリックできる
+        // clickBtnの真偽値がtrueの時だけクリックできる
         clickBtn = true;
-      }, 3100);
+      }, 2100);
     } else {
       return false;
     }
@@ -48,25 +46,23 @@
   // 次へボタンをクリック時
   next.addEventListener('click', function () {
     if (clickBtn === true) {
-      // clickBtnの真偽地がfalseの時だけクリックできない
+      // clickBtnの真偽値がfalseの時だけクリックできない
       clickBtn = false;
       // スライドショーのクラス（appear）を追加
       view.classList.add("anime");
       // current＋１つ増;
       current++;
       // currentの数値が画像の枚数より多くなったりしないようにif文で分岐
-      if (current > setImage.length - 1) {
-        // 上限まで行くと０に戻す
-        current = 0;
-      }
+      //上限まで行くと０に戻す
+      if  (current > setImage.length-1) current = 0;
       // currentをインデックス番号として変数setImageの配列からcurrentのインデックス番号の画像を取り出す
       view.src = setImage[current];
       // スライドショーのクラス（anime）を外す
-      setTimeout('view.classList.remove("anime");', 3100);
+      setTimeout('view.classList.remove("anime");', 2100);
       setTimeout(function () {
-        // clickBtnの真偽地がtrueの時だけクリックできる
+        // clickBtnの真偽値がtrueの時だけクリックできる
         clickBtn = true;
-      }, 3100);
+      }, 2100);
     } else {
       return false;
     }
